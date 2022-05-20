64a435641da7f8a3715efff42942934ff8c73434
thudxz <liux.bio@gmail.com>
2022-05-20 15:07:03 +0800

    220520 install org.eg.dbs directly in rdiffbase

--- rdiffbase/code/installBasicPackagesScript.R
+++ rdiffbase/code/installBasicPackagesScript.R
@@ -1,6 +1,6 @@
 source("./code/installPackages.R")
 
-packagesFromBioconductor = c("limma", "DESeq2", "Biobase")
+packagesFromBioconductor = c("limma", "DESeq2", "Biobase", "org.Hs.eg.db", "org.Mm.eg.db", "org.Rn.eg.db")
 packagesFromCran = c("plumber", "dplyr", "purrr", "rjson")
 
 lapply(packagesFromBioconductor, installOneBiocPackage)
